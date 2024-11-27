93456c5749ff50e933aec950168f77c692d361d3
Sathaporn24 <Sathaporn.Cabkaew.24@gmail.com>
2024-11-27 23:29:57 +0700

    Gun Update

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -14,6 +14,9 @@ public class PlayerController : MonoBehaviour
     public float nextJumpPress = 0.0f;
     public float fireRate = 0.2f;
     public float nextFireRate = 0.0f;
+    private int comboStep = 0;
+    private float comboTimer = 0f;
+    public float comboResetTime = 1f; // เวลาที่จะรีเซ็ตคอมโบถ้าไม่มีการกดเพิ่ม
     private Rigidbody2D ridigBody2D;
     private Physics2D physic2D;
     Animator animator;
@@ -69,13 +72,59 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("Jump",false);
         }
 
-        if(Input.GetKey(KeyCode.X) && Time.time > nextFireRate){
-            nextFireRate = Time.time + fireRate;
-            animator.SetBool("Attack1",true);
-            Attack();
-        }else{
-            animator.SetBool("Attack1",false);
-        }
+        if (Input.GetKeyDown(KeyCode.X) && Time.time > nextFireRate)
+{
+    nextFireRate = Time.time + fireRate;
+    HandleAttack();
+}
+
+void HandleAttack()
+{
+    comboStep++;
+    comboTimer = 0f; // รีเซ็ตตัวจับเวลา
+
+    if (comboStep == 1)
+    {
+        animator.ResetTrigger("Attack2");
+        animator.ResetTrigger("Attack3");
+        animator.SetTrigger("Attack1");
+    }
+    else if (comboStep == 2)
+    {
+        animator.ResetTrigger("Attack1");
+        animator.ResetTrigger("Attack3");
+        animator.SetTrigger("Attack2");
+    }
+    else if (comboStep == 3)
+    {
+        animator.ResetTrigger("Attack1");
+        animator.ResetTrigger("Attack2");
+        animator.SetTrigger("Attack3");
+        ResetCombo(); // รีเซ็ตคอมโบหลังจากท่าสุดท้าย
+    }
+}
+
+// ฟังก์ชันสำหรับรีเซ็ตคอมโบเมื่อหมดเวลา
+void ResetCombo()
+{
+    comboStep = 0;
+    comboTimer = 0f;
+    animator.ResetTrigger("Attack1");
+    animator.ResetTrigger("Attack2");
+    animator.ResetTrigger("Attack3");
+    animator.SetInteger("ComboStep", 0);
+}
+
+// ใน Update เพิ่มการตรวจจับเวลารีเซ็ตคอมโบ
+if (comboStep > 0)
+{
+    comboTimer += Time.deltaTime;
+    if (comboTimer > comboResetTime)
+    {
+        ResetCombo();
+    }
+}
+
     }
 
     //void TakeDamage(int damage){
@@ -90,4 +139,15 @@ public class PlayerController : MonoBehaviour
         yield return new WaitForSeconds(0.3f);
         Instantiate(hitArea,transform.position,transform.rotation);
     }
+
+    void OnTriggerEnter2D (Collider2D other){
+        if(other.gameObject.tag == "health"){
+            healthbar = healthbar + 50;
+            Destroy(other.gameObject);
+        }
+        if(other.gameObject.tag == "deathzone"){
+            healthbar = 0;
+            //Destroy(other.gameObject);
+        }
+    }    
 }
