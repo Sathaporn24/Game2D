ab2999b016f228d06c2fecd02115fd4db611e869
Padiphan5940 <padiphan.prem@gmail.com>
2024-11-28 23:16:24 +0700

    UpdateScene
    Add Music MainMenu
    Add Music Stage 1
    Add Sfx Button
    Fix Main Menu
    Add Menu Navigation

---
+++ Assets/MainMenuNav.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainMenuNavigate : MonoBehaviour
+{
+    public void play()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+}
